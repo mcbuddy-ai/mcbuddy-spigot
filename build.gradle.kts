8989dc8e5a8247f312bf3a3966aafbbe33dcfda3
Pavel Erokhin <mairwunnx@gmail.com>
2025-07-20 02:37:32 +0300

    Добавлены файлы конфигурации Gradle, включая .gitignore, build.gradle.kts, gradle.properties, скрипты gradlew и gradlew.bat, а также настройки для Gradle Wrapper. Эти изменения обеспечивают поддержку сборки проекта на Kotlin и интеграцию с Spigot API.

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+  kotlin("jvm") version "2.2.0"
+  kotlin("plugin.serialization") version "2.2.0"
+  id("com.gradleup.shadow") version "8.3.0"
+  id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+group = "ru.mcbuddy"
+version = "1.1.0"
+
+repositories {
+  mavenCentral()
+  maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
+    name = "spigotmc-repo"
+  }
+  maven("https://oss.sonatype.org/content/groups/public/") {
+    name = "sonatype"
+  }
+}
+
+dependencies {
+  compileOnly("org.spigotmc:spigot-api:1.21.4-R0.1-SNAPSHOT")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+  implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
+
+tasks {
+  runServer {
+    // Configure the Minecraft version for our task.
+    // This is the only required configuration besides applying the plugin.
+    // Your plugin's jar (or shadowJar if present) will be used automatically.
+    minecraftVersion("1.21")
+  }
+}
+
+val targetJavaVersion = 21
+kotlin {
+  jvmToolchain(targetJavaVersion)
+}
+
+tasks.build {
+  dependsOn("shadowJar")
+}
+
+tasks.processResources {
+  val props = mapOf("version" to version)
+  inputs.properties(props)
+  filteringCharset = "UTF-8"
+  filesMatching("plugin.yml") {
+    expand(props)
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "mcbuddy-spigot"
